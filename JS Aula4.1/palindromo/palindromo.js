b732b9381481702937738a5f3c2402fccd3f4207
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 19:16:10 -0300

    Update palindromo.js

--- JS Aula4.1/palindromo/palindromo.js
+++ JS Aula4.1/palindromo/palindromo.js
@@ -9,15 +9,12 @@ palindromo('x') // true
 
 
 function palindromo(s) {
-    if (s.length <= 1) {
-        console.log(`${s} é Palíndromo`)
-    } else {
-        for (let i = 0, j = s.length - 1; i < j; i++, j--) {
-            if (s.charAt(i) != s.charAt(j)) {
-                console.log(`${s} não é palíndromo`)
-                return
-            }
+    for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+        if (s.charAt(i) != s.charAt(j)) {
+            console.log(`${s} não é palíndromo`)
+            return
         }
     }
+
     console.log(`${s} é palíndromo`)
 }
