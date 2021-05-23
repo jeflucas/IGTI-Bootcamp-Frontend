bab67839e4adf1251fa72b049a0f5234837f2d84
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 19:46:22 -0300

    aula

--- DOM Aula5.1/base/script.js
+++ DOM Aula5.1/base/script.js
@@ -1,1 +1,8 @@
-var numero1 = document.getElementById(numero1)
+var numero1 = document.getElementById('numero1')
+var numero2 = document.getElementById('numero2')
+
+var somar = (numero1, numero2) => {
+    resultado = numero1 + numero2
+    document.getElementById('resultado').innerHTML = resultado
+    
+}
