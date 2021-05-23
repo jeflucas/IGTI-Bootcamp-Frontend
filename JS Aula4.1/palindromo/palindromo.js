4fee138289594a490b0f3866eb69ac0fe01824c5
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 18:27:22 -0300

    desafio

---
+++ JS Aula4.1/palindromo/palindromo.js
@@ -0,0 +1,13 @@
+palindromo('aba') // true
+palindromo('radar') // true
+palindromo('xyzzyx') // true
+palindromo('xyzzyx') // true
+palindromo('bola') // false
+palindromo('abcddcb') // false
+palindromo('') // true
+palindromo('x') // true
+
+
+function palindromo(s) {
+    
+}
