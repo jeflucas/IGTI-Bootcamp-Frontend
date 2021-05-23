cf1db94d93e954e976e4290cde3784abdfa996ce
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 20:48:50 -0300

    aula

--- DOM Aula5.1/base/script.js
+++ DOM Aula5.1/base/script.js
@@ -1,8 +1,8 @@
 var numero1 = document.getElementById('numero1')
 var numero2 = document.getElementById('numero2')
+var r = document.getElementById('resultado')
 
-var somar = (numero1, numero2) => {
-    resultado = numero1 + numero2
-    document.getElementById('resultado').innerHTML = resultado
-    
-}
+document.addEventListener('input', () => {
+    var resultado = numero1.valueAsNumber + numero2.valueAsNumber
+    r.textContent = isNaN(resultado) ? '' : resultado
+})
