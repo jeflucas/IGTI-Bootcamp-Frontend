c17a026d20043e886c2efe544b075e9a6916aed2
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 22:07:26 -0300

    Update script.js

--- DOM Aula5.1/criando-elementos/script.js
+++ DOM Aula5.1/criando-elementos/script.js
@@ -6,11 +6,29 @@ for (var i = 0; i < fsList.length; i++) {
 function initMultipleFielSet (fs) {
     var addButton = document.createElement("button")
     addButton.textContent = "Adicionar"
+    addButton.type = "button"
 
     fs.appendChild(addButton)
-    
+
+    var firstInput = fs.querySelector("input")
+
     addButton.addEventListener("click", function(){
+        var div = document.createElement("div")
         var newImput = document.createElement("input")
-        fs.appendChild(newImput)
+        newImput.name = firstInput.name
+        newImput.type = firstInput.type
+
+        var deleteButton = document.createElement("button")
+        deleteButton.textContent = 'Excluir'
+        deleteButton.type = 'button'
+
+        div.appendChild(newImput)
+        div.appendChild(deleteButton)
+
+        deleteButton.addEventListener("click", function() {
+            div.remove()
+        })
+
+        fs.insertBefore(div, addButton)
     })
 }
