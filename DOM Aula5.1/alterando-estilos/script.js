1ced07619c869727d838535b3f934dc90f7d64f8
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 22:30:50 -0300

    Update script.js

--- DOM Aula5.1/alterando-estilos/script.js
+++ DOM Aula5.1/alterando-estilos/script.js
@@ -4,4 +4,16 @@ function setPos(x, y) {
     caixa.style.top = y + px
     caixa.style.lef = x + px
 
-} 
+}
+
+caixa.addEventListener("mousedown", iniciaArraste)
+
+function iniciaArraste(event) {
+    caixa.className = "arrastando"
+}
+
+document.addEventListener("mouseup", terminaArraste)
+
+function terminaArraste (event) {
+    caixa.className = ""
+}
