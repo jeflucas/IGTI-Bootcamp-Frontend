910eab2a308883cca282d297aa5acb826ebbec2e
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-24 13:27:32 -0300

    Update script.js

--- HTTP Req Aula 8/01 - fetch api/script.js
+++ HTTP Req Aula 8/01 - fetch api/script.js
@@ -1,19 +1,41 @@
-let employeesPromise = fetch("http://localhost:3000/employees")
-//
-
-employeesPromise.then((resposta) => {
-    resposta.json().then((arrayEmployees) => {
-
-        let table = renderTable(arrayEmployees);
-        document.getElementById("app").innerHTML = table
+function solution1() {
+    let employeesPromise = fetch("http://localhost:3000/employees")
+    //
+    employeesPromise.then((r1) => {
+        r1.json().then((employees) => {
+            let rolesPromise = fetch("http://localhost:3000/roles")
+            rolesPromise.then(r2 => {
+                r2.json().then(roles => {
+                    let table = renderTable(employees, roles);
+                    document.getElementById("app").innerHTML = table
+                })
+            })
+        })
     })
+}
+//solution1()
 
-})
-
-function renderTable(arrayEmployees) {
-    let rows = arrayEmployees.map(employee => {
-        return `<tr><td>${employee.id}</td><td>${employee.name}</td></tr>`
+function solution2() {
+    fetch("http://localhost:3000/employees").then((r1) => {
+        return r1.json()
     })
+        .then((employees) => {
+            fetch("http://localhost:3000/roles").then(r2 => {
+                return r2.json()
+            })
+                .then(roles => {
+                    let table = renderTable(employees, roles);
+                    document.getElementById("app").innerHTML = table
+                })
+        })
+}
 
-    return `<table> ${rows.join(" ")} </table>`
+solution2()
+
+function renderTable(employees, roles) {
+    let rows = employees.map((employee) => {
+        let role = roles.find((role) => role.id == employee.role_id)
+        return `<tr><td>${employee.id}</td><td>${employee.name}</td><td>${role.name}</td></tr>`
+    })
+    return `<table> ${rows.join("")} </table>`
 }
