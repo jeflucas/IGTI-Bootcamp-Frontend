7f4b4ee67953a5e93647de7ef6de7c84de114cfc
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-23 22:48:36 -0300

    Update script.js

--- DOM Aula5.1/alterando-estilos/script.js
+++ DOM Aula5.1/alterando-estilos/script.js
@@ -1,8 +1,9 @@
 var caixa = document.getElementById("caixa");
+var cx, cy
 
 function setPos(x, y) {
-    caixa.style.top = y + px
-    caixa.style.lef = x + px
+    caixa.style.left  = x + "px"
+    caixa.style.top = y + "px"
 
 }
 
@@ -10,12 +11,24 @@ caixa.addEventListener("mousedown", iniciaArraste)
 document.addEventListener("mouseup", terminaArraste)
 
 function iniciaArraste(event) {
+    cx = event.clientX - pxParaNum(caixa.style.left)
+    cy = event.clientY - pxParaNum(caixa.style.top)
+
     caixa.classList.add("arrastando")
     document.addEventListener("mousemove", arrasta)
 }
 
-
-
 function terminaArraste (event) {
     caixa.classList.remove("arrastando")
+    document.removeEventListener("mousemove", arrasta)
+}
+
+function arrasta(event) {
+    var x = event.clientX
+    var y = event.clientY
+    setPos(x - cx, y - cy)
+}
+
+function pxParaNum(s) {
+    return +(s.replace('px' , ''))
 }
