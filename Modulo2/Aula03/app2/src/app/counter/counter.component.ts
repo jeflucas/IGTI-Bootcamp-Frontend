4c17ce688954e9dd4276a2e8abdc4d1ae19b176d
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-04 14:00:09 -0300

    aula

---
+++ Modulo2/Aula03/app2/src/app/counter/counter.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+
+  titulo: string = 'contador'
+
+}
