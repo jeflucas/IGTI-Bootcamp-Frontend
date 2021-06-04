c4678624c68f051d7847bf4a34a30a74dc749856
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-04 17:23:52 -0300

    estudo

--- Modulo2/Aula03/ticTacToeAula/src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ Modulo2/Aula03/ticTacToeAula/src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -5,11 +5,58 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './jogo-da-velha.component.html',
   styleUrls: ['./jogo-da-velha.component.css']
 })
-export class JogoDaVelhaComponent implements OnInit {
+export class JogoDaVelhaComponent {
 
-  constructor() { }
+  currentPlayer: string = 'X'
+  winner: string = ''
 
-  ngOnInit(): void {
+  board: string[][] = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
+
+  processPlay(line: number, col: number) {
+    if(this.board[line][col] == '' && this.winner == '') {
+      this.board[line][col] = this.currentPlayer
+
+      //verifica ganhador
+      if (this.checkWinner(this.currentPlayer)){
+        this.winner = this.currentPlayer
+      }
+
+      //trocar jogador
+      if (this.currentPlayer == 'X') {
+        this.currentPlayer = 'O'
+      } else {
+        this.currentPlayer = 'X'
+      }
+    }
+  }
+
+  checkWinner(player: string): boolean {
+    for (let i = 0; i < this.board.length; i++){
+      if (this.board[i][0] == player && this.board[i][1] == player && this.board[i][2] == player) {
+        return true
+      }
+    }
+    for (let i = 0; i < this.board.length; i++){
+      if (this.board[0][i] == player && this.board[1][i] == player && this.board[2][0] == player) {
+        return true
+      }
+    }
+    return false
+  }
+
+  reset(){
+    this.currentPlayer = 'X'
+    this.winner = ''
+
+    this.board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
   }
 
 }
