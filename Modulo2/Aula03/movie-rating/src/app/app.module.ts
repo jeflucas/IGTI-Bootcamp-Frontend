7bc374d58d7a7516790452880778fa97f92e6dde
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-07 21:57:29 -0300

    aula

--- Modulo2/Aula03/movie-rating/src/app/app.module.ts
+++ Modulo2/Aula03/movie-rating/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RatingComponent } from './rating/rating.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RatingComponent
+    RatingComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
