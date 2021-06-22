e942457600ec5070fdc6fa823000590a213be7e7
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-22 20:40:35 -0300

    estudo

---
+++ Modulo3/desafio1/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            title: 'BRL to USD',
+            subtitle: 'A siple curency conversor',
+            logo: 'https://www.svgrepo.com/show/59997/dollar-sign.svg'
+        }
+    },
+
+})
+
+app.mount('#app')
