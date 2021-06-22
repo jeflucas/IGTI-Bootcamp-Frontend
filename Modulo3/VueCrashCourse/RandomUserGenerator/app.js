9001d89dac40722dd75f8365ac2484b89c2d3044
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-22 20:22:58 -0300

    estudo

--- Modulo3/VueCrashCourse/RandomUserGenerator/app.js
+++ Modulo3/VueCrashCourse/RandomUserGenerator/app.js
@@ -9,12 +9,17 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        getUser(){
-            this.firstName = 'Xerxes',
-            this.lastName = 'Silva',
-            this.email = 'xexeudasgatas@hotmail.co.uk',
-            this.gender = 'male',
-            this.picture = 'https://randomuser.me/api/portraits/men/21.jpg'
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            //console.log(results)
+
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
         }
     },
 })
