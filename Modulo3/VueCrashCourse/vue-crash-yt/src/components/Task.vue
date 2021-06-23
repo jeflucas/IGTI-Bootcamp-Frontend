afe0d0084acc265c5d1f9fe1086cf03bc2cfad42
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-23 14:04:59 -0300

    estudo

--- Modulo3/VueCrashCourse/vue-crash-yt/src/components/Task.vue
+++ Modulo3/VueCrashCourse/vue-crash-yt/src/components/Task.vue
@@ -2,7 +2,7 @@
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>
       {{ task.text }}
-      <i @click="onDelete(taks.id)" class="fas fa-times"></i>
+      <i @click="onDelete(task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
       onDelete(id) {
-          console.log(id)
+          this.$emit('delete-task', id)
       }
   },
 };
