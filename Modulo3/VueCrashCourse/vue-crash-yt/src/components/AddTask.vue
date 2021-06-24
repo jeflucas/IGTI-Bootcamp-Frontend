4f40a316c6c22da6a4bb5343fff8d48d018f6fba
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-24 16:42:40 -0300

    edstudo

---
+++ Modulo3/VueCrashCourse/vue-crash-yt/src/components/AddTask.vue
@@ -0,0 +1,18 @@
+<template>
+    <form class="add-form">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text" name="text" placeholder="Add Task" />
+        </div>
+        <div class="form-control">
+            <label>Day and Time</label>
+            <input type="text" name="day" placeholder="Add Day and Time" />
+        </div>
+        <div class="form-control">
+            <label>Set Reminder</label>
+            <input type="checkbox" name="reminder" />
+        </div>
+
+        <input type="submit" value="Save Task" class="btn btn-block" />
+    </form>
+</template>
