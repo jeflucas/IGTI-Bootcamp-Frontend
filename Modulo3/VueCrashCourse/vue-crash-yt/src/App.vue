903344f038265c4e099ef89b5bd4c663603effd0
jeflucas <jef.lucas@hotmail.co.uk>
2021-06-24 17:08:06 -0300

    estudo

--- Modulo3/VueCrashCourse/vue-crash-yt/src/App.vue
+++ Modulo3/VueCrashCourse/vue-crash-yt/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <div>
+    <AddTask @add-task="addTask" />
+    </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -8,19 +11,27 @@
 <script>
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
   name: "App",
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false
     };
   },
   methods: {
+
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
+
     deleteTask(id) {
       if(confirm('Are you sure?')){
         this.tasks = this.tasks.filter((task) => task.id !== id)
